3ebf750f2e708bc9717ea479ebb40d2e90f90a69
MalkavianBilbao <MalkavianBilbao@users.noreply.github.com>
2020-05-09 19:35:53 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     char name[20]; /* I prefer to use standard C instead of cs50.h library altought scanf is not considered secure without limiting it*/
     printf("What's your name: ");
-    scanf("%s",name);
-    printf("Hello, %s!\n",name);
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
 }
