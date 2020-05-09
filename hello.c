e3a54606be6d4289c7772ae7ea06a4d7b2cb9f8f
MalkavianBilbao <MalkavianBilbao@users.noreply.github.com>
2020-05-09 20:03:16 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <string.h> /* I prefer to use standard C instead of cs50.h library */
 
 int main(void)
 {
-    char name[20]; /* I prefer to use standard C instead of cs50.h library altought scanf is not considered secure without limiting it*/
+    char name[20];
     printf("What's your name: ");
-    scanf("%s", name);
+//    scanf("%s", name);
+    fgets(name, sizeof(name), stdin);
+    /* strcspn counts the number of characters until it hits a '\r' or a '\n' (in other words, it finds the first '\r' or '\n'). If it doesn't hit anything, it stops at the '\0' (returning the length of the string). */
+    name[strcspn(name, "\r\n")] = 0;
     printf("Hello, %s!\n", name);
 }
